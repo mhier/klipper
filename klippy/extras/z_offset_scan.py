c9ced61f446da9df54bc3a10e3bdc5e9d26eb015
Martin Hierholzer <mhier@tartaros>
2020-12-23 14:26:53 +0100

    keep scanned z offset correct even when bed mesh is rescanned

--- klippy/extras/z_offset_scan.py
+++ klippy/extras/z_offset_scan.py
@@ -10,6 +10,7 @@ class ZOffsetScan:
     def __init__(self, config):
         self.name = config.get_name()
         self.printer = config.get_printer()
+        self.bed_mesh = self.printer.lookup_object('bed_mesh')
 
         self.speed = config.getfloat('speed', 5., above=0.)
 
@@ -19,6 +20,9 @@ class ZOffsetScan:
             desc=self.cmd_Z_OFFSET_SCAN_help)
 
         self.z_offset = config.getfloat('z_offset', 0.)
+        self.mesh_offset = config.getfloat('mesh_offset', 0.)
+        self.at_x = config.getfloat('at_x', 0.)
+        self.at_y = config.getfloat('at_y', 0.)
 
         # Register transform
         gcode_move = self.printer.load_object(config, 'gcode_move')
@@ -30,7 +34,6 @@ class ZOffsetScan:
     def cmd_Z_OFFSET_SCAN(self, gcmd):
         toolhead = self.printer.lookup_object('toolhead')
         probe = self.printer.lookup_object('probe')
-        bed_mesh = self.printer.lookup_object('bed_mesh')
         configfile = self.printer.lookup_object('configfile')
 
         # perform probe measurement
@@ -42,11 +45,18 @@ class ZOffsetScan:
 
         # subtract bed mesh value at the measurement position (otherwise we get
         # the bed mesh correction twice)
-        corr = bed_mesh.get_mesh().calc_z(x,y) + bed_mesh.get_mesh().mesh_offset
+        corr = self.bed_mesh.get_mesh().calc_z(x,y) + \
+            self.bed_mesh.get_mesh().mesh_offset
         self.z_offset -= corr
+        self.mesh_offset = corr
+        self.at_x = x
+        self.at_y = y
 
         # update config file
         configfile.set(self.name, 'z_offset', self.z_offset)
+        configfile.set(self.name, 'mesh_offset', self.mesh_offset)
+        configfile.set(self.name, 'at_x', self.at_x)
+        configfile.set(self.name, 'at_y', self.at_y)
         gcmd.respond_info("New z_offset: %f\n"
                   "The SAVE_CONFIG command will update the printer config\n"
                   "file and restart the printer" % (self.z_offset))
@@ -60,7 +70,9 @@ class ZOffsetScan:
 
     def move(self, newpos, speed):
         x, y, z, e = newpos
-        self.normal_transform.move([x, y, z+self.z_offset, e], speed)
+        corr = self.bed_mesh.get_mesh().calc_z(self.at_x,self.at_y) + \
+            self.bed_mesh.get_mesh().mesh_offset - self.mesh_offset
+        self.normal_transform.move([x, y, z+self.z_offset-corr, e], speed)
 
 
 def load_config(config):
